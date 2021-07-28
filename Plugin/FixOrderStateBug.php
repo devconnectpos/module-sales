7505aa6fc0b41e3afd718e2dbe12c969979972f8
Khue Quang Nguyen <khuenq@connectpos.com>
2021-07-28 17:47:27 +0700

    Release 21.07.28

---
+++ Plugin/FixOrderStateBug.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace SM\Sales\Plugin;
+
+
+use Magento\Framework\Math\FloatComparator;
+use Magento\Sales\Model\Order;
+
+class FixOrderStateBug
+{
+    /**
+     * @var FloatComparator
+     */
+    private $comparator;
+
+    /**
+     * @param FloatComparator $comparator
+     */
+    public function __construct(FloatComparator $comparator)
+    {
+        $this->comparator = $comparator;
+    }
+
+    /**
+     * Checks if Credit Memo is available with Store Credit.
+     *
+     * @see Order::canCreditmemo()
+     * @param Order $subject
+     * @param boolean $result
+     * @return boolean
+     */
+    public function afterCanCreditmemo(Order $subject, bool $result): bool
+    {
+        // process a case only if credit memo can be created
+        if (!$result) {
+            return $result;
+        }
+
+        // process a case only if reward points or customer balance were refunded
+        if ($subject->getBaseRwrdCrrncyAmtRefunded() === null
+            && $subject->getBaseCustomerBalanceRefunded() === null
+        ) {
+            return $result;
+        }
+
+        $totalInvoiced = $subject->getBaseTotalInvoiced()
+            + $subject->getBaseRwrdCrrncyAmtInvoiced()
+            + $subject->getBaseCustomerBalanceInvoiced()
+            + $subject->getBaseGiftCardsInvoiced();
+        $totalRefunded = $subject->getBaseTotalRefunded()
+            + $subject->getBaseRwrdCrrncyAmntRefnded()
+            + ($subject->getBaseCustomerBalanceInvoiced() ? $subject->getBaseCustomerBalanceRefunded() : 0)
+            + $subject->getBaseGiftCardsRefunded();
+
+        if ($this->comparator->greaterThan($totalInvoiced, $totalRefunded)) {
+            return true;
+        }
+
+        if ($this->comparator->greaterThanOrEqual($totalRefunded, (float)$subject->getBaseTotalPaid())) {
+            return false;
+        }
+
+        return true;
+    }
+}
