c86027729028bd346c85821742b55ab036b9edbe
Thaitt1 <thaitt1@smartosc.com>
2024-04-12 10:51:24 +0700

    Release 24.04.12

--- Plugin/CollectionPlugin.php
+++ Plugin/CollectionPlugin.php
@@ -2,8 +2,25 @@
 
 namespace SM\Sales\Plugin;
 
+use Magento\Framework\Webapi\Rest\Request;
+
 class CollectionPlugin
 {
+    /**
+     * @var Request
+     */
+    protected $request;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(
+        Request $request
+    )
+    {
+        $this->request = $request;
+    }
+
     /**
      * @param $subject
      * @param $result
@@ -11,11 +28,18 @@ class CollectionPlugin
      */
     public function afterGetItemsByColumnValue($subject, $result)
     {
-        foreach ($subject->load() as $item) {
-            if ($item->getData("sku") == "custom_sales_product_for_retail") {
-                return [];
+        if (!$this->request->getRequestUri()) {
+            return $result;
+        }
+        if (!strpos("xretail/load-order-data", $this->request->getRequestUri())
+            || !strpos("xretail/save-order", $this->request->getRequestUri())) {
+            foreach ($subject->load() as $item) {
+                if ($item->getData("sku") == "custom_sales_product_for_retail") {
+                    return [];
+                }
             }
         }
         return $result;
     }
 }
+
